e37c5488a1b370dd15f18732dde0c73d055fcfa0
German <germanarevalojerez@gmail.com>
2021-09-21 15:25:48 -0500

    middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,18 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { createUser } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
 router.post(
   '/new',
-  [check('name', 'Name is required').not().isEmpty()],
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'email is required').isEmail(),
+    check('password', 'password is required').not().isEmpty(),
+    validateFields,
+  ],
   createUser
 );
 

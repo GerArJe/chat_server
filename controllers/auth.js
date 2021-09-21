026e259531f68df7a00f264add714d8ce662ff51
German <germanarevalojerez@gmail.com>
2021-09-21 16:56:04 -0500

    validate fields

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,14 +2,25 @@ const { response } = require('express');
 const User = require('../models/user');
 
 const createUser = async (req, res = response) => {
-  const user = new User(req.body);
+  const { email } = req.body;
+  try {
+    const existEmail = await User.findOne({ email });
+    if (existEmail) {
+      return res.status(400).json({ ok: false, msg: 'Email already exists' });
+    }
 
-  await user.save();
+    const user = new User(req.body);
 
-  res.json({
-    ok: true,
-    body: req.body,
-  });
+    await user.save();
+
+    res.json({
+      ok: true,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ ok: false, msg: 'Error server' });
+  }
 };
 
 module.exports = { createUser };

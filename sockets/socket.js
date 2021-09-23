e548d63c0c79815240787e3a410662a0e736c221
German <germanarevalojerez@gmail.com>
2021-09-23 17:08:11 -0500

    authenticate client

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,17 @@
 const { io } = require('../index');
+const { checkJWT } = require('../helpers/jwt');
 
 // Mensajes de Sockets
 io.on('connection', (client) => {
   console.log('Cliente conectado');
+
+  const [valid, uid] = checkJWT(client.handshake.headers['x-token']);
+
+  if (!valid) {
+    return client.disconnect();
+  }
+  
+  console.log('Client authenticated');
 
   client.on('disconnect', () => {
     console.log('Cliente desconectado');
